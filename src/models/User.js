0505c83ca0e25cf1445a1dc678309ad5b3900e49
nradkova <neliradkova@abv.bg>
2021-10-19 00:34:26 +0300

    user model added

---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+const {Schema,model}=require('mongoose');
+
+const schema=new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+module.exports=model('User',schema);
