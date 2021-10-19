a9cd0f106178d3d18a4ff13b75eb69bd34183754
nradkova <neliradkova@abv.bg>
2021-10-20 00:03:30 +0300

    housingService finished

--- src/services/housingService.js
+++ src/services/housingService.js
@@ -6,6 +6,45 @@ async function createHousing({ name, type, year, city, image, description, piece
     return housing;
 }
 
+async function getHousingById(id) {
+    const housing = await Housing.findById(id)
+        .populate('rented')
+        .populate('creator')
+        .lean();
+    if (housing) {
+        const viewModel = {
+            _id: housing._id,
+            name: housing.name,
+            type:housing.type,
+            year:housing.year,
+            city:housing.city,
+            image: housing.image,
+            description: housing.description,
+            pieces: housing.pieces,
+            rented: housing.rented,
+            ownerId:cube.owner._id.toString()
+        }
+        return viewModel;
+    }
+    return undefined;
+}
+
+async function edit(id, housing) {
+    const current = await Housing.findById(id);
+    if (!current) {
+        throw new ReferenceError('No such data');
+    }
+    Object.assign(current, housing);
+    return current.save();
+}
+
+async function del(id) {
+    const current = await Housing.findById(id);
+    if (!current) {
+        throw new ReferenceError('No such data');
+    }
+  return Housing.deleteOne({_id:id});
+}
 
 async function getAllHousings() {
     return await Housing.find({}).lean();
@@ -18,6 +57,9 @@ async function getLastThreeHousings() {
 
 module.exports = {
     createHousing,
+    getHousingById,
+    edit,
+    del,
     getAllHousings,
     getLastThreeHousings,
 }
